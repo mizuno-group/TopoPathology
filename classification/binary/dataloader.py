2b73adab702c60febcc1552d0dcb7e06ad959d88
groovy-phazuma <phazuma19980625@gmail.com>
2023-07-15 23:46:36 +0900

    hiegm6 update

--- classification/binary/dataloader.py
+++ classification/binary/dataloader.py
@@ -37,3 +37,23 @@ def collect_data(bin_path='/workspace/Pathology_Graph/230712_cg_classification/r
     
     return data_list
 
+def collect_multi_data(path_list=['/workspace/mnt/data1/Azuma/Pathology/centroids_based_graph/0_N/cell_graphs/train','/workspace/mnt/data1/Azuma/Pathology/centroids_based_graph/6_IC/cell_graphs/train']):
+    data_list = []
+    for path in path_list:
+        bin_path = path+'/*.bin'
+        l = glob.glob(bin_path)
+        for path in tqdm(l):
+            g_list, label_dict = load_graphs(path)
+            edge_info = g_list[0].edges()
+            node_feature = g_list[0].ndata['feat']
+
+            x = torch.tensor(node_feature, dtype=torch.float)
+            edge_index = torch.tensor([np.array(edge_info[0]),np.array(edge_info[1])])
+            label = label_dict['label']
+            if int(label) == 6:
+                label = 1
+            else:
+                label = 0
+            d = Data(x=x,edge_index=edge_index.contiguous(),t=label)
+            data_list.append(d)
+    return data_list
